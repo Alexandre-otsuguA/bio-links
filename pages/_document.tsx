5f1c195e9478683cbbfca786aad41f529e04fc5d
Alexandre Augusto <aleaugusto.dev@gmail.com>
2022-04-06 19:33:22 -0300

    Moved google font import to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,14 @@ export default class MyDocument extends Document {
     return (
       <Html lang='pt-br'>
         <Head>
+          {/* Fonts */}
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=swap'
+            rel='stylesheet'
+          />
+
           <style
             id='stitches'
             dangerouslySetInnerHTML={{ __html: getCssText() }}
--- src/components/global/Head.tsx
+++ src/components/global/Head.tsx
@@ -7,13 +7,6 @@ const Head = () => (
       rel='icon'
       href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🕹️</text></svg>'
     />
-    {/* Fonts */}
-    <link rel='preconnect' href='https://fonts.googleapis.com' />
-    <link rel='preconnect' href='https://fonts.gstatic.com' />
-    <link
-      href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=swap'
-      rel='stylesheet'
-    />
     {/* Buy me a coffe */}
     <script
       defer
